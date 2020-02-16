422c99df9e43a3b69a9f1a1496781a5231a8d4de
Praveen <praveen.raghuvanshii@gmail.com>
2020-02-16 13:34:34 +0530

    formatted code

--- src/main/java/com/ashraya/customer/domain/LoginMapper.java
+++ src/main/java/com/ashraya/customer/domain/LoginMapper.java
@@ -12,12 +12,10 @@ public class LoginMapper {
         return GoogleAccountInfo.builder().firstName(payload.getFirstName()).lastName(payload.getLastName()).displayName(payload.getFirstName()).emailId(payload.getEmailId())
                         .emailVerified(payload.isEmailVerified()).mobileNumber(payload.getPhoneNumber()).googlePictureUrl(payload.getGooglePictureUrl()).build();
     }
-
     public static FacebookAccountInfo mapToFacebookAccountInfo(LoginRequestPayload payload) {
         return FacebookAccountInfo.builder().displayName(payload.getFirstName()).firstName(payload.getFirstName()).lastName(payload.getLastName()).emailId(payload.getEmailId())
                         .emailVerified(payload.isEmailVerified()).facebookPictureUrl(payload.getFacebookPictureUrl()).mobileNumber(payload.getPhoneNumber()).build();
     }
-
     public static WaterRecipient mapToWaterRecipient(LoginRequestPayload payload, FacebookAccountInfo facebookAccountInfo, GoogleAccountInfo googleAccountInfo, Category category) {
         return WaterRecipient.builder().facebookAccountInfo(facebookAccountInfo).googleAccountInfo(googleAccountInfo).gstNumber(payload.getGstNumber())
                         .mobileNumber(payload.getPhoneNumber()).name(payload.getDisplayName()).appUsage(payload.getAppUsage()).category(category)
--- src/main/java/com/ashraya/customer/util/SMSUtil.java
+++ src/main/java/com/ashraya/customer/util/SMSUtil.java
@@ -40,7 +40,7 @@ public class SMSUtil {
         }
     }
     
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         System.out.println(sendOtpMessage(1234,"7804814213"));
-    }
+    }*/
 }
